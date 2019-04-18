a9362c48dcba6d48e0ec209928d95da8e8302ee0
Pedro Gomes <pedrogomes94@gmail.com>
2019-04-18 16:58:58 +0200

    getChainData

--- src/helpers/utilities.ts
+++ src/helpers/utilities.ts
@@ -112,6 +112,21 @@ export function getChainData(chainId: number): IChainData {
     throw new Error("ChainId missing or not supported");
   }
 
+  const API_KEY = process.env.REACT_APP_INFURA_PROJECT_ID;
+
+  if (
+    chainData.rpc_url.includes("infura.io") &&
+    chainData.rpc_url.includes("%API_KEY%") &&
+    API_KEY
+  ) {
+    const rpcUrl = chainData.rpc_url.replace("%API_KEY%", API_KEY);
+
+    return {
+      ...chainData,
+      rpc_url: rpcUrl
+    };
+  }
+
   return chainData;
 }
 
