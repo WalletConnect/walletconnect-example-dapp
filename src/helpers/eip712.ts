7f0fb82993ef285aaf437b86aef5d15264caaa42
Pedro Gomes <pedrogomes94@gmail.com>
2023-06-27 10:40:10 +0200

    new EIP-712 payload (#112)

--- src/helpers/eip712.ts
+++ src/helpers/eip712.ts
@@ -3,45 +3,30 @@ const example = {
     EIP712Domain: [
       { name: "name", type: "string" },
       { name: "version", type: "string" },
+      { name: "chainId", type: "uint256" },
       { name: "verifyingContract", type: "address" },
     ],
-    RelayRequest: [
-      { name: "target", type: "address" },
-      { name: "encodedFunction", type: "bytes" },
-      { name: "gasData", type: "GasData" },
-      { name: "relayData", type: "RelayData" },
-    ],
-    GasData: [
-      { name: "gasLimit", type: "uint256" },
-      { name: "gasPrice", type: "uint256" },
-      { name: "pctRelayFee", type: "uint256" },
-      { name: "baseRelayFee", type: "uint256" },
-    ],
-    RelayData: [
-      { name: "senderAddress", type: "address" },
-      { name: "senderNonce", type: "uint256" },
-      { name: "relayWorker", type: "address" },
-      { name: "paymaster", type: "address" },
+    Permit: [
+      { name: "owner", type: "address" },
+      { name: "spender", type: "address" },
+      { name: "value", type: "uint256" },
+      { name: "nonce", type: "uint256" },
+      { name: "deadline", type: "uint256" },
     ],
   },
   domain: {
-    name: "GSN Relayed Transaction",
-    version: "1",
+    name: "Liquid staked Ether 2.0",
+    version: "2",
     chainId: 1,
-    verifyingContract: "0x6453D37248Ab2C16eBd1A8f782a2CBC65860E60B",
+    verifyingContract: "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84",
   },
-  primaryType: "RelayRequest",
+  primaryType: "Permit",
   message: {
-    target: "0x9cf40ef3d1622efe270fe6fe720585b4be4eeeff",
-    encodedFunction:
-      "0xa9059cbb0000000000000000000000002e0d94754b348d208d64d52d78bcd443afa9fa520000000000000000000000000000000000000000000000000000000000000007",
-    gasData: { gasLimit: "39507", gasPrice: "1700000000", pctRelayFee: "70", baseRelayFee: "0" },
-    relayData: {
-      senderAddress: "0x22d491bde2303f2f43325b2108d26f1eaba1e32b",
-      senderNonce: "3",
-      relayWorker: "0x3baee457ad824c94bd3953183d725847d023a2cf",
-      paymaster: "0x957F270d45e9Ceca5c5af2b49f1b5dC1Abb0421c",
-    },
+    owner: "0x01Badd833a247cABf0c2da49a61d06eCb1b7E993",
+    spender: "0x889edC2eDab5f40e902b864aD4d7AdE8E412F9B1",
+    value: "10000978944812",
+    nonce: "0x0",
+    deadline: "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
   },
 };
 
